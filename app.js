e1b335b5c211f1f6e6a825ef600e1aa267feb3e0
Rsja12 <asocarras10@gmail.com>
2020-04-23 13:55:24 -0400

    create a TODO list

---
+++ app.js
@@ -0,0 +1,10 @@
+/* TODO
+
+add event handler for submit btn
+get input values
+add item from input into our data struct
+add item from data struct to UI 
+calculate budget
+update UI from budget calculation
+
+*/
