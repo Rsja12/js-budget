e8cc43bb3b75fef1ed40fc42074ffc1754616e28
Rsja12 <asocarras10@gmail.com>
2020-04-24 11:11:55 -0400

    create Expense function constructor in budgetController

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ Modules:
 
 var budgetController = (function() {
 
-    
+    var Expense = function(id, description, value) {
+        this.id = id,
+        this.description = description,
+        this.value = value 
+    }
 
 })()
 
